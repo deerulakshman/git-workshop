aedb5ad0e50ee2d87f1aacb2cf7dd770433f56e4
exam2 <exam2@RVRCSE.LOCAL>
2024-06-29 09:42:30 +0530

    SQL commiting

--- update.sql
+++ update.sql
@@ -1,1 +1,9 @@
-rayugzsuygzuihio
+CREATE table_account(
+id INT PRIMSRY KEY
+balance DECIMAL(18, 2)
+);
+INSERT INTO accounts (id,balance)
+VALUES (1, 10000000.00);
+UPDATE accounts
+SET balance= balance- 20000.00;
+SELECT * FROM accounts;
